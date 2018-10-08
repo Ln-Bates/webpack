7522beec66417f550eedc248f821289ee2bc71f3
ln-bates <421009569@qq.com>
2018-10-08 12:08:24 +0800

    first commit

--- template/src/main.js
+++ template/src/main.js
@@ -2,12 +2,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 {{/if_eq}}
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import App from './App';
+import VueAxios from 'vue-axios';
+import Axios from 'axios';
 {{#router}}
-import router from './router'
+import router from './router';
 {{/router}}
 
+{{#vuex}}  //vuex为true的时候就会写入这些
+import store from  './store';
+{{/vuex}}
+
+import './assets/css/app.scss';
+
+Vue.use(VueAxios, Axios);
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
@@ -16,6 +25,9 @@ new Vue({
   {{#router}}
   router,
   {{/router}}
+  {{#vuex}}
+  store,
+  {{/vuex}}
   {{#if_eq build "runtime"}}
   render: h => h(App)
   {{/if_eq}}
@@ -23,4 +35,4 @@ new Vue({
   components: { App },
   template: '<App/>'
   {{/if_eq}}
-})
+});
